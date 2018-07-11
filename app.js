288201ebeb28b0d3b785088f27e2365c53324afe
junfeisu <1982764150@qq.com>
2018-07-11 10:40:58 +0800

    simplify the path handle

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const fs = require('fs')
+const path = require('path')
+
 const ignorePwds = []
 const staticResourceTable = {}
 const staticFileReg = /^.+(\.js|\.css|\.png|\.jpg|\.jpeg|\.svg|\.ttf|\.scss|\.less)$/i
 
 let rootPwd = process.argv[3]
-if (rootPwd[rootPwd.length - 1] !== '/') {
-  rootPwd = rootPwd + '/'
-}
 let relativePwd = rootPwd
 let specifiedName = ''
 
@@ -17,7 +16,7 @@ const parseArgs = (args) => {
 
   for (let i = 4; i < args.length; i++) {
     if (args[i] === '-r') {
-      relativePwd = rootPwd + args[++i]
+      relativePwd = path.resolve(rootPwd, args[++i])
     } else if (args[i] === '-i') {
       ignorePwds.push(args[++i])
     } else if (args[i] === '-n') {
@@ -30,25 +29,22 @@ const next = (pwd) => {
   const files = fs.readdirSync(pwd)
 
   files.forEach((file, index) => {
-    const isDirectory = fs.statSync(pwd + '/' + file).isDirectory()
+    const isDirectory = fs.statSync(path.resolve(pwd, file)).isDirectory()
     if (isDirectory) {
       if (!ignorePwds.includes(file)) {
-        if (pwd[pwd.length - 1] !== '/') {
-          pwd += '/'
-        }
-        next(pwd + file + '/')
+        next(path.resolve(pwd, file))
       }
     } else {
       if (staticFileReg.test(file)) {
-        if (pwd === rootPwd) {
+        if (path.resolve(pwd) === path.resolve(rootPwd)) {
           staticResourceTable[file] = '/' + file
         } else {
-          staticResourceTable[pwd.replace(rootPwd, '') + file] = pwd.replace(rootPwd, '/') + file
+          staticResourceTable[path.join(pwd.replace(rootPwd, ''), file)] = path.join(pwd.replace(rootPwd, '/'), file)
         }
       }
       if (pwd === relativePwd && index === files.length - 1) {
         let fileName = specifiedName ? specifiedName + '.json' : 'staticResource.json' 
-        fs.open(`${rootPwd}` + fileName, 'w+', null, (e, fd) => {
+        fs.open(path.resolve(rootPwd, fileName), 'w+', null, (e, fd) => {
           if (e) {
             console.log('create json file err', e)
             return
